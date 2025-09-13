fe3b69bbaa5e213dd27a1a6a04e972f5bf071247
katiaschmitt <katiaschmittp@gmail.com>
2025-09-12 21:31:44 -0300

    Adicionar arquivos de projeto.

---
+++ JovemProgramadorWeb/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using JovemProgramadorWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JovemProgramadorWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ValidaUsuario(Usuario usuario)
+        {
+            try
+            {
+                if(usuario.Email == "katia@gmail" && usuario.Senha == "1234")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["MsgErro"] = "Usuário ou senha incorretos! Tente novamente...";
+                }
+            }
+            catch (Exception)
+            {
+
+                TempData["MsgErro"] = "Erro ao buscar dados do usuário";
+            }
+
+            return View("Index");
+        }
+
+    }
+}
